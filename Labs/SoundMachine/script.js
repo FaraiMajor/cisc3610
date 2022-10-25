7275b6c993399def17acc7094a97ebe5d0b3b6e3
Farai Major <faraimutukumira@gmail.com>
2022-10-25 18:57:22 -0400

    sound machine

---
+++ Labs/SoundMachine/script.js
@@ -0,0 +1,29 @@
+const car = document.getElementById("car");
+const playCar = () => {
+    console.log("play car");
+    car.play();
+};
+
+const church = document.getElementById("church");
+const playChurch = () => {
+    console.log("play church bell");
+    church.play();
+};
+
+const football = document.getElementById("football");
+const playFootball = () => {
+    console.log("play football");
+    football.play();
+};
+
+const guitar = document.getElementById("guitar");
+const playGuitar = () => {
+    console.log("play guitar");
+    guitar.play();
+};
+
+const piano = document.getElementById("piano");
+const playPiano = () => {
+    console.log("play piano");
+    piano.play();
+};
