4d2930a034db959eb69d1d35d9664b8af6ec93ee
Farai Major <faraimutukumira@gmail.com>
2022-10-29 18:05:10 -0400

    speech recognition

--- Assignments/VoiceRecognition/script.js
+++ Assignments/VoiceRecognition/script.js
@@ -2,53 +2,75 @@ var recognition = new(window.SpeechRecognition || window.webkitSpeechRecognition
 recognition.lang = 'en-US';
 recognition.interimResults = false;
 
-const canvas = document.getElementById("mycanvas");
+var canvas = document.getElementById("mycanvas");
 var ctx = canvas.getContext("2d");
 const button = document.getElementById("speakBtn");
 
 const speakMessage = () => {
-    if (button.innerHTML === "Speak") {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        recognition.start();
-        // Change button value to "Stop" once clicked
-        button.innerHTML = "Stop";
+        if (button.innerHTML === "Speak") {
+            // ctx.clearRect(0, 0, canvas.width, canvas.height);
+            recognition.start();
+            // Change button value to "Stop" once clicked
+            button.innerHTML = "Stop";
 
-    } else if (button.innerHTML === "Stop") {
-        recognition.stop();
-        button.innerHTML = "Speak";
+        } else if (button.innerHTML === "Stop") {
+            recognition.stop();
+            button.innerHTML = "Speak";
+        }
     }
-}
+    // function to speak instructions
 const speak = (text) => {
     const utterance = new SpeechSynthesisUtterance(text);
     window.speechSynthesis.speak(utterance);
 };
 
+cssColorNames = ["aliceblue", "antiquewhite", "aqua", "aquamarine", "azure", "beige", "bisque", "black", "blanchedalmond", "blue", "blueviolet", "brown", "burlywood", "cadetblue", "chartreuse", "chocolate", "coral", "cornflowerblue", "cornsilk", "crimson", "cyan", "darkblue", "darkcyan", "darkgoldenrod", "darkgray", "darkgrey", "darkgreen", "darkkhaki", "darkmagenta", "darkolivegreen", "darkorange", "darkorchid", "darkred", "darksalmon", "darkseagreen", "darkslateblue", "darkslategray", "darkslategrey", "darkturquoise", "darkviolet", "deeppink", "deepskyblue", "dimgray", "dimgrey", "dodgerblue", "firebrick", "floralwhite", "forestgreen", "fuchsia", "gainsboro", "ghostwhite", "gold", "goldenrod", "gray", "grey", "green", "greenyellow", "honeydew", "hotpink", "indianred", "indigo", "ivory", "khaki", "lavender", "lavenderblush", "lawngreen", "lemonchiffon", "lightblue", "lightcoral", "lightcyan", "lightgoldenrodyellow", "lightgray", "lightgrey", "lightgreen", "lightpink", "lightsalmon", "lightseagreen", "lightskyblue", "lightslategray", "lightslategrey", "lightsteelblue", "lightyellow", "lime", "limegreen", "linen", "magenta", "maroon", "mediumaquamarine", "mediumblue", "mediumorchid", "mediumpurple", "mediumseagreen", "mediumslateblue", "mediumspringgreen", "mediumturquoise", "mediumvioletred", "midnightblue", "mintcream", "mistyrose", "moccasin", "navajowhite", "navy", "oldlace", "olive", "olivedrab", "orange", "orangered", "orchid", "palegoldenrod", "palegreen", "paleturquoise", "palevioletred", "papayawhip", "peachpuff", "peru", "pink", "plum", "powderblue", "purple", "rebeccapurple", "red", "rosybrown", "royalblue", "saddlebrown", "salmon", "sandybrown", "seagreen", "seashell", "sienna", "silver", "skyblue", "slateblue", "slategray", "slategrey", "snow", "springgreen", "steelblue", "tan", "teal", "thistle", "tomato", "transparent", "turquoise", "violet", "wheat", "white", "whitesmoke", "yellow", "yellowgreen"];
+
+// Deafault color and size
+var currentRadius = 25;
+var currentColor = 'black'
+var X = canvas.width / 2;
+var Y = canvas.height / 2;
+ctx.beginPath();
+ctx.arc(X, Y, currentRadius, 0, 2 * Math.PI, false);
+ctx.fillStyle = currentColor;
+ctx.fill()
+ctx.stroke();
+
+// speech commands
 recognition.onresult = function(event) {
-    var message = event.results[0][0].transcript;
-    if (message != NaN) {
-        draw((message));
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        var message = event.results[0][0].transcript;
+        if (message >= 25 && message <= 300) {
+            draw(message, currentColor);
+            currentRadius = message;
+        } else if (message < 25) {
+            speak('Size too small, the minimum size is 25.')
 
-        // } else if (message === '100') {
-        //     draw(message);
+        } else if (message > 300) {
+            speak('Sorry, but the size limit is 300.')
 
-    } else {
-        // Write unknown on canvas
-        ctx.font = 'bold 28px sans-serif';
-        ctx.fillText('Unknown', 70, 50);
+        } else if (cssColorNames.includes(message)) {
+            draw(currentRadius, message);
+            currentColor = message;
 
-        ctx.font = 'bold 12px sans-serif';
-        ctx.fillText('You said: ' + event.results[0][0].transcript, 70, 110);
+        } else
+        if (message === "help") {
+            speak("Say a color to change the circle's color or say a radius of size between 25 and 300");
+        } else if (message === "about") {
+            speak("Farai Major, Copyright 2022.");
+        } else {
+            draw(currentRadius, 'black')
+        }
+        console.log('You said: ', event.results[0][0].transcript);
     }
-    console.log('You said: ', event.results[0][0].transcript);
-}
-
-function draw(color) {
-    var X = canvas.width / 2;
-    var Y = canvas.height / 2;
-    var R = color;
+    // function to change radius and color
+function draw(r, color) {
+    X = canvas.width / 2;
+    Y = canvas.height / 2;
     ctx.beginPath();
-    ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
-    ctx.fillStyle = "black";
+    ctx.arc(X, Y, r, 0, 2 * Math.PI, false);
+    ctx.fillStyle = color;
     ctx.fill()
     ctx.stroke();
 }
